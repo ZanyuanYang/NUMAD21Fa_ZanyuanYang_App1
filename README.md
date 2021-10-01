1a2076f31c0a729f1d5bbd37671139498f3137ca
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-10-01 11:07:56 -0700

    Add: add snackbar and launch url in web browser

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 2. A2 Cognitive Teardown
 
 3. A3 Clicky Clicky
-    - 6 buttons
-    - This criterion is linked to a Learning OutcomeText shows what is expected when a button is pressed
-    - This criterion is linked to a Learning OutcomeText shows what is expected when a button is not pressed.
+   - 6 buttons
+   - This criterion is linked to a Learning OutcomeText shows what is expected when a button is pressed
+   - This criterion is linked to a Learning OutcomeText shows what is expected when a button is not pressed.
+
+4. A4 Link Collector
+   - Add `Floating` button
+   - Create modal with two inputs: one is `name`, one is `url`
+   - Add the input the list
+   - Make link clickable and launch it in web browser
--- app/src/main/java/com/example/numad21fa_zanyuanyang/recyclerAdapter.java
+++ app/src/main/java/com/example/numad21fa_zanyuanyang/recyclerAdapter.java
@@ -13,19 +13,31 @@ import java.util.ArrayList;
 public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyViewHolder>{
 
     private ArrayList<LinkList> linkList;
+    private OnLinkListener mOnLinkListener;
 
-    public recyclerAdapter(ArrayList<LinkList> linkList){
+    public recyclerAdapter(ArrayList<LinkList> linkList, OnLinkListener onLinkListener){
         this.linkList = linkList;
+        this.mOnLinkListener = onLinkListener;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView nameText;
         private TextView urlText;
+        OnLinkListener onLinkListener;
 
-        public MyViewHolder(final View view){
+        public MyViewHolder(final View view, OnLinkListener onLinkListener){
             super(view);
             nameText = view.findViewById(R.id.name);
             urlText = view.findViewById(R.id.url);
+            this.onLinkListener = onLinkListener;
+
+//            itemView.setOnClickListener(this);
+            urlText.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onLinkListener.onLinkClick(getAdapterPosition());
         }
     }
 
@@ -33,7 +45,7 @@ public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyView
     @Override
     public recyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.link_collector_list, parent, false);
-        return new MyViewHolder(itemView);
+        return new MyViewHolder(itemView, mOnLinkListener);
     }
 
     @Override
@@ -50,8 +62,12 @@ public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyView
     }
 
 
-    public String getItemName(int position){
+    public String getItemUrl(int position){
         return linkList.get(position).getUrl();
+    }
+
+    public interface OnLinkListener{
+        void onLinkClick(int position);
     }
 
 }
